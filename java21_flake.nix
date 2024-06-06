4d62d9a683da5017796d5e6cbc74a390d946f00b
Gary Baldrich Pinedo <46449103+icodecauseiloveit@users.noreply.github.com>
2024-06-06 14:19:13 -0500

    Create java21_flake.nix

---
+++ java21_flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Development environment for Java applications.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        jdk21
+      ];
+    };
+  };
+}
